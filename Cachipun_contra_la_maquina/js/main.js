d1cf7c01c4218260b5b6b0348070b2bfe9e65378
carolina lara <lalarita@hotmail.com>
2021-06-17 13:33:37 -0400

    cachipún sigue

--- Cachipun_contra_la_maquina/js/main.js
+++ Cachipun_contra_la_maquina/js/main.js
@@ -3,52 +3,45 @@ var respuesta = prompt("¿Quieres jugar Cachipún?: Si o No");
 if (respuesta == "Si" || respuesta == "si" || respuesta == "SI") {
 
 } else {
-    alert("¡¡sniff sniff!!");
+    alert("¡¡sniff (╥﹏╥) sniff!!");
 }
 
 
 var numero = prompt("cuántas veces quieres jugar");
+var numero = parseInt(numero);
+document.write("<br> <h1>Jugaras</h1>" + numero + "veces");
+for (var i = 0; i < numero; i++) {
+    function jugar(usuario) {
+        var maquina = maquina;
+        var opcion = ["piedra", "papel", "tijera"];
 
+        maquina = Math.floor(Math.random() * 3);
+        opcionUsuario = opcion[usuario];
+        opcionMaquina = opcion[maquina]
+        document.write("<h3>Elegiste</h3>" + opcion[usuario]);
+        document.write("<br> <h3>La máquina eligió</h3>" + opcion[maquina]);
 
-function jugar(usuario) {
-    var maquina = maquina;
-    var opcion = ["piedra", "papel", "tijera"];
-    maquina = Math.floor(Math.random() * 3);
-    opcionUsuario = opcion[usuario];
-    opcionMaquina = opcion[maquina]
-    document.write(opcion[usuario]);
-    document.write(opcion[maquina]);
+        if (opcionUsuario === opcionMaquina) {
+            document.write("<br> <h1>(ɔ◔‿◔)ɔ ♥EMPATARON♥(^◡^ )</h1>");
+        }
+        if (opcionUsuario === "piedra" && opcionMaquina === "papel") {
+            document.write("<br> <h1>¡PERDISTE! ☜(ˆ▽ˆ)</h1>");
+        }
+        if (opcionUsuario === "piedra" && opcionMaquina === "tijera") {
+            document.write("<br> <h1>¡GANASTE! (>‿◠)✌</h1>");
+        }
+        if (opcionUsuario === "papel" && opcionMaquina === "tijera") {
+            document.write("<br> <h1>¡PERDISTE! ☜(ˆ▽ˆ)</h1>");
+        }
+        if (opcionUsuario === "papel" && opcionMaquina === "piedra") {
+            document.write("<br> <h1>¡GANASTE! (>‿◠)✌</h1>");
+        }
+        if (opcionUsuario === "tijera" && opcionMaquina === "piedra") {
+            document.write("<br> <h1>¡PERDISTE! ☜(ˆ▽ˆ)</h1>");
+        }
+        if (opcionUsuario === "tijera" && opcionMaquina === "papel") {
+            document.write("<br> <h1>¡GANASTE! (>‿◠)✌</h1>");
+        }
 
-    if (opcionUsuario === opcionMaquina) {
-        document.write("empate");
-    } else if (opcionUsuario === 0) {
-        if (opcionMaquina === 2);
-        document.write("ganador");
-    } else if (opcionUsuario === 1) {
-        if (opcionMaquina === 2);
-        document.write("perdedor");
-        if (opcionMaquina === 0)
-            document.write("ganador");
-    } else if (opcionUsuario === 2) {
-        if (opcionMaquina === 0);
-        document.write("perdedor");
     }
-    if (opcionMaquina === 1);
-    document.write("ganador");
 }
-
-
-
-
-
-/*switch (resultado) {
-    case "empate":
-        document.write("empate");
-        break
-    case "ganador":
-        document.write("ganaste");
-        breack
-    case "perdedor":
-        document.write("perdiste");
-        breack
-}*/
