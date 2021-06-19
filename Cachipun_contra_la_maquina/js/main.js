9912b90b16d5b8f0fbafc3b85a4bf515cffe78ab
carolina lara <lalarita@hotmail.com>
2021-06-18 21:16:23 -0400

    fin Cachipun

--- Cachipun_contra_la_maquina/js/main.js
+++ Cachipun_contra_la_maquina/js/main.js
@@ -2,52 +2,57 @@
 var respuesta = prompt("¿Quieres jugar Cachipún?: Si o No");
 
 if (respuesta == "Si" || respuesta == "si" || respuesta == "SI") {
+    /*Consulta al usuario cuántas veces va a jugar*/
     var numero = parseInt(prompt("Cuántas veces quieres jugar"));
-
 } else {
     alert("¡¡sniff (╥﹏╥) sniff!!");
 }
-
-/*Consulta al usuario cuántas veces va a jugar*/
-
 document.querySelector('#jugadas').innerHTML = numero;
+var contador = 0;
+
 /*Repetición de jugadas según lo señala el usuario*/
-for (var i = 0; i < numero; i++) {
-    function jugar(usuario) {
-        var maquina = maquina;
-        var opcion = ["piedra", "papel", "tijera"];
+function jugar(usuario) {
 
-        maquina = Math.floor(Math.random() * 3);
-        opcionUsuario = opcion[usuario];
-        opcionMaquina = opcion[maquina];
-        document.querySelector('#opcion-u').innerHTML = opcion[usuario];
-        document.querySelector('#opcion-m').innerHTML = opcion[maquina];
-        /*Resultados, según las opciones de los jugadores*/
+    var opcion = ["piedra", "papel", "tijera"];
+    var maquina = maquina;
 
-        if (opcionUsuario === opcionMaquina) {
-            document.querySelector('#resultado').innerHTML = "(ɔ◔‿◔)ɔ ♥EMPATARON♥(^◡^ )";
-        }
-        if (opcionUsuario === "piedra" && opcionMaquina === "papel") {
-            document.querySelector('#resultado').innerHTML = "¡PERDISTE! ☜(ˆ▽ˆ)";
-        }
-        if (opcionUsuario === "piedra" && opcionMaquina === "tijera") {
-            document.querySelector('#resultado').innerHTML = "¡GANASTE! (>‿◠)✌";
-        }
-        if (opcionUsuario === "papel" && opcionMaquina === "tijera") {
-            document.querySelector('#resultado').innerHTML = "¡PERDISTE! ☜(ˆ▽ˆ)";
-        }
-        if (opcionUsuario === "papel" && opcionMaquina === "piedra") {
-            document.querySelector('#resultado').innerHTML = "¡GANASTE! (>‿◠)✌";
-        }
-        if (opcionUsuario === "tijera" && opcionMaquina === "piedra") {
-            document.querySelector('#resultado').innerHTML = "¡PERDISTE! ☜(ˆ▽ˆ)";
-        }
-        if (opcionUsuario === "tijera" && opcionMaquina === "papel") {
-            document.querySelector('#resultado').innerHTML = "¡GANASTE! (>‿◠)✌";
-        }
+    maquina = Math.floor(Math.random() * 3);
 
+    opcionUsuario = opcion[usuario];
+    opcionMaquina = opcion[maquina];
+    document.querySelector('#opcion-u').innerHTML = opcion[usuario];
+    document.querySelector('#opcion-m').innerHTML = opcion[maquina];
+    /*Resultados, según las opciones de los jugadores*/
+
+    if (opcionUsuario === opcionMaquina) {
+        document.querySelector('#resultado').innerHTML = "(ɔ◔‿◔)ɔ ♥EMPATARON♥(^◡^ )";
     }
-    if (i == numero) {
+    if (opcionUsuario === "piedra" && opcionMaquina === "papel") {
+        document.querySelector('#resultado').innerHTML = "¡PERDISTE! ☜(ˆ▽ˆ)";
+    }
+    if (opcionUsuario === "piedra" && opcionMaquina === "tijera") {
+        document.querySelector('#resultado').innerHTML = "¡GANASTE! (>‿◠)✌";
+    }
+    if (opcionUsuario === "papel" && opcionMaquina === "tijera") {
+        document.querySelector('#resultado').innerHTML = "¡PERDISTE! ☜(ˆ▽ˆ)";
+    }
+    if (opcionUsuario === "papel" && opcionMaquina === "piedra") {
+        document.querySelector('#resultado').innerHTML = "¡GANASTE! (>‿◠)✌";
+    }
+    if (opcionUsuario === "tijera" && opcionMaquina === "piedra") {
+        document.querySelector('#resultado').innerHTML = "¡PERDISTE! ☜(ˆ▽ˆ)";
+    }
+    if (opcionUsuario === "tijera" && opcionMaquina === "papel") {
+        document.querySelector('#resultado').innerHTML = "¡GANASTE! (>‿◠)✌";
+    }
+    document.getElementById("jugadas").innerHTML = ++contador;
+    if (contador == numero) {
         document.querySelector('#fin').innerHTML = "(ﾟ∀ﾟ)☞ ¡fin de tus jugadas!";
+        if (contador >= numero) {
+            document.getElementById('boton').disabled = true;
+            document.getElementById('boton1').disabled = true;
+            document.getElementById('boton2').disabled = true;
+        }
     }
+
 }
