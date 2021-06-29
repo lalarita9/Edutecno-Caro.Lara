0583c531ac5100b271a7e95a54b9d461772e3dae
carolina lara <lalarita@hotmail.com>
2021-06-29 19:20:50 -0400

    agregando html

--- Ricomida/assets/js/main.js
+++ Ricomida/assets/js/main.js
@@ -1,10 +1,10 @@
 //Alerta cuando el usuario hace un click en enviar por correo.
 const $enviar = document.getElementById("enviar");
 $enviar.addEventListener('click', () => {
-        alert("El correo fue enviado correctamente");
-    })
-    //cambio texto título color rojo doble click
+    alert("El correo fue enviado correctamente");
+});
+//cambio texto título color rojo doble click
 let on = document.querySelector('#on');
 let on1 = document.querySelector('#on1');
-on.addEventListener('dblclick', () => on.style.color = '#ff0b0b')
-on1.addEventListener('dblclick', () => on1.style.color = '#ff0b0b')
+on.addEventListener('dblclick', () => on.style.color = '#ff0b0b');
+on1.addEventListener('dblclick', () => on1.style.color = '#ff0b0b');
