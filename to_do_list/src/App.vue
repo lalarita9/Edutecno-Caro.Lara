d9fe952b3bc98f40323304bb47ca0b0635357fd6
Carolina Lara <lalarita@hotmail.com>
2021-09-07 22:37:17 -0300

    Agregando metodos para la lista

---
+++ to_do_list/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+<div id="app">
+    <h3 id="title">{{titulo}}</h3>
+Tarea:
+<input type="text" placeholder="Ingresa nueva tarea" v-model="nuevatarea"/>
+
+<button type="button" name="button" @click="agregarlista">Crear</button>
+ 
+<hr>
+        <h3 id="sub">{{subtitulo}}</h3>
+<ul>
+<li v-for="(lista, index) in listas" :key="index">
+<span>{{ index }} : {{ lista.tarea }}</span>
+</li>
+</ul>
+</div>
+</template>
+ 
+<script>
+
+export default {
+name: 'App',
+data() {
+return {
+titulo: 'Crea una nueva tarea',
+subtitulo: 'Lista',
+
+listas: [{
+    
+    tarea:'Terminar el dasafío',
+},],
+nuevatarea:'',
+}
+},
+ methods: {
+     
+                agregarlista: function() {
+                                    
+					if (this.nuevatarea!= ''){
+                        this.listas.push({tarea:this.nuevatarea, 
+
+					})
+
+                    } else {
+                        alert('Por favor ingrese una tarea'
+                            )
+                    }
+                    
+	
+				this.nuevatarea='';
+                }
+            },
+          
+};
+</script>
+<style scoped>
+#app {
+    color:black;
+    font-size: 20px;
+    text-align: center;
+    font-family: 'Century Gothic';
+}
+#title, #sub {
+color: #dfe2d2;
+font-size: 30px;
+
+}
+li {
+    list-style-position: inside;
+}
+</style>
