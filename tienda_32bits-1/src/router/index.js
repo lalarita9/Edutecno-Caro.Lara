2698676fbcba63993cc4e0ead3c1732c08d66d77
Carolina Lara <lalarita@hotmail.com>
2021-09-16 22:44:27 -0300

    agregando archivos

--- tienda_32bits-1/src/router/index.js
+++ tienda_32bits-1/src/router/index.js
@@ -1,29 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Inicio from '../views/Inicio.vue'
+import Busquedas from '../views/Busquedas.vue'
+import Ventas from '../views/Ventas.vue'
+import Total from '../views/Total.vue'
+
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'inicio',
+        component: Inicio
+    },
+    {
+        path: '/busquedas',
+        name: 'busquedas',
+        component: Busquedas
+    },
+    {
+        path: '/ventas',
+        name: 'ventas',
+        component: Ventas
+    },
+    {
+        path: '/total',
+        name: 'total',
+        component: Total
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ tienda_32bits-1/src/views/Busquedas.vue
@@ -0,0 +1,14 @@
+<template>
+  <listado></listado>  
+</template>
+
+<script>
+import Listado from '../components/Listado.vue'
+export default {
+  name: 'Busquedas',
+  components: {
+    Listado,
+  },
+  
+}
+</script>
