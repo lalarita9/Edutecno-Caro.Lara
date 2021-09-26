17644cc1b36f33b00549930b918b62f674bdb7cb
Carolina Lara <lalarita@hotmail.com>
2021-09-26 19:00:05 -0300

    agregando metdo before

--- creacion_autentificacion_vue-router/src/router/index.js
+++ creacion_autentificacion_vue-router/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import { getAuth } from 'firebase/auth'
 Vue.use(VueRouter)
 
 const routes = [{
@@ -27,21 +27,21 @@ const routes = [{
 ]
 
 const router = new VueRouter({
-        mode: 'history',
-        base: process.env.BASE_URL,
-        routes
-    })
-    /*router.beforeEach((to, from, next) => {
-        let user = Firebase.auth().currentUser;
-        let authRequired = to.matched.some(route => route.meta.login);
-        if (!user && authRequired) {
-            next('/');
-        } else if (user && !authRequired) {
-            next('/');
-        } else {
-            next();
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(route => route.meta.authRequired)) {
+        if (getAuth().currentUser) {
+            next({
+                path: '/'
+            });
         }
-    });*/
+    } else {
+        next();
+    }
+});
 
 
 export default router
